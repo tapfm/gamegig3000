6cb7e5ad7bbe4a00a63709dcb574fa9bfac7e26c
Nathan Corbyn <me@nathancorbyn.com>
2017-11-30 18:40:33 +0000

    Initial commit

---
+++ src/net/industiral/src/Main.java
@@ -0,0 +1,23 @@
+package net.industrial.src;
+
+import net.industrial.grassland.Game;
+import net.industrial.grassland.GrasslandException;
+
+public class Main extends Game {
+    public Main() {
+        super("Game Gig 3000", 800, 600, false);
+    }
+
+    @Override
+    public void initStates() 
+            throws GrasslandException {
+        addState(new IndustrialGames());
+        enterState(0);
+    }
+
+    public static void main(String args[]) 
+            throws GrasslandException {
+        Game game = new Main();
+        game.init();
+    }
+}
